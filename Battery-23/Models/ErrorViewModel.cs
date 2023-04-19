c3ba6d172876b3c8c7e3cdcca7214f6abde369ab
Pl. Petroff <plpetroff@gmail.com>
2023-04-20 01:48:15 +0300

    Add project files.

---
+++ Battery-23/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+namespace Battery_23.Models
+{
+    using System;
+
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
