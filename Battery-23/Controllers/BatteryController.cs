78beac6e59626b4e295707c3dcd3595403edd8ee
Pl. Petroff <plpetroff@gmail.com>
2023-04-24 12:17:03 +0300

    repearing Add-view

---
+++ Battery-23/Controllers/BatteryController.cs
@@ -0,0 +1,20 @@
+﻿namespace Battery_23.Controllers
+{
+    using Battery_23.Models.Batteries;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BatteryController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddBatteryFormModel battery)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Battery-23/Models/Batteries/AddBatteryFormModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Battery_23.Models.Batteries
+{
+    using Battery_23.Data.Models;
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Linq;
+
+    public class AddBatteryFormModel
+    {
+        [Display(Name = "Brand of the Battery")]
+        public int BrandId { get; init; }
+        public Brand Brand { get; init; } 
+    }
+}
