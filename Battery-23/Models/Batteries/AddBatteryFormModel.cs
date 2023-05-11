fb3a86498552c3d5f1b9c98a06dd075956bc6db6
Pl. Petroff <plpetroff@gmail.com>
2023-05-11 07:43:34 +0300

    11,05,2023

--- Battery-23/Models/Batteries/AddBatteryFormModel.cs
+++ Battery-23/Models/Batteries/AddBatteryFormModel.cs
@@ -8,6 +8,20 @@
     {
         [Display(Name = "Brand of the Battery")]
         public int BrandId { get; init; }
-        public Brand Brand { get; init; } 
+        public Brand Brand { get; init; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int TechnologyId { get; set; }
+        public Technology Technology { get; set; }
+
+        public int CapacityId { get; set; }
+        public Capacity Capacity { get; set; }
+
+        public int TerminalId { get; set; }
+        public Terminal Terminal { get; set; }
+
+        public int BoxTypeId { get; set; }
+        public BoxType BoxType { get; set; }
     }
 }
